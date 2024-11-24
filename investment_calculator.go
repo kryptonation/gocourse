1d45ecda328f4e4d8525266ddfbdcfe70f2fc207
DD <mailbox@drdilip.in>
2024-11-24 05:39:32 +0530

    Simple data type declaration

--- investment_calculator.go
+++ investment_calculator.go
@@ -7,10 +7,10 @@ import (
 
 func main() {
 	fmt.Println("Investment Calculator")
-	var investmentAmount = 1000
+	var investmentAmount float64 = 1000
 	var roi = 5.5
-	var years = 10
+	var years float64 = 10
 
-	var futureValue = float64(investmentAmount) * math.Pow(1 + roi/100, float64(years))
+	var futureValue = investmentAmount * math.Pow(1 + roi/100, years)
 	fmt.Println(futureValue)
 }
